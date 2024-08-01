48508eaa601d2574f353631ed1f6e16738635292
Oyematth01 <oyematth@gmail.com>
2024-08-01 04:11:07 +0100

    task0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const fs = require('fs');
+import { readFile } from 'fs';
 
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
+readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
